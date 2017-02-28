709d662fc048428ee3c0089962c3fbe7b89dc245
Prashant-SK <presso96@gmail.com>
2017-02-27 17:24:34 -0800

    first commit

--- src/com/example/sprint1/EventDetailsActivity.java
+++ src/com/example/sprint1/EventDetailsActivity.java
@@ -1,49 +1,22 @@
 package com.example.sprint1;
 
-
-
-//import com.example.hello.MainActivity.OnClick;
-
 import android.app.Activity;
-import android.content.Intent;
-//import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class EventDetailsActivity extends Activity {
 
-	Button setup;
-	Button login;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.event_details);
 	}
-	/*
-	public void onLoginClick(View v){
-		if(v.getId() == R.id.login_button){
-			Intent i = new Intent(MainActivity.this, Login.class);
-			startActivity(i);
-		}
-		
-	}
-	
-	public void onSignup1Click(View v){
-		if(v.getId() == R.id.signUp1){
-			Intent i = new Intent(MainActivity.this, Signup.class);
-			startActivity(i);
-		}
-	}
-	*/
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.event_details, menu);
 		return true;
 	}
 
@@ -58,5 +31,4 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
 }
